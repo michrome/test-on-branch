da986f58d0bfef1ea5f656a3cc08920a03edff6c
Mike Jerome <michrome@users.noreply.github.com>
2024-04-17 11:16:06 +0100

    Create table if it doesn't exist

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 import assert from "node:assert";
 import test from "node:test";
-import { addUser, getUserCount } from "./lib/database.js";
+import { addUser, createUserTable, getUserCount } from "./lib/database.js";
 
 test("that INSERT added 1 row", async () => {
+  await createUserTable();
   const before = await getUserCount();
   await addUser();
   const after = await getUserCount();
